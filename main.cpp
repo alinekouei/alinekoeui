440f01c1a13cea1e72052d4481028711c849a595
alinekouei <4285aali@gmail.com>
2025-02-22 23:26:07 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int fibonacci(int n) {
-    if (n <= 1) {
-        return n;
-    } else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-}
+// تعریف ساختار Person
+struct Person {
+    string name;
+    int age;
+    double height;
+};
 
 int main() {
-    int number;
-    cout << "Enter a number: ";
-    cin >> number;
+    // ایجاد یک نمونه از ساختار Person
+    Person person1;
 
-    cout << "Fibonacci number " << number << " is " << fibonacci(number) << endl;
+    // مقداردهی به اعضای ساختار
+    person1.name = "Ali";
+    person1.age = 25;
+    person1.height = 1.78;
+
+    // چاپ اطلاعات شخص
+    cout << "Name: " << person1.name << endl;
+    cout << "Age: " << person1.age << endl;
+    cout << "Height: " << person1.height << " meters" << endl;
 
     return 0;
 }
